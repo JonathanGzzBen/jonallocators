3697892bbe84821b42000b6470e94fcf3966171a
Jonathan Gonzalez Benavides <jonathangzzben@gmail.com>
2025-12-09 11:50:51 -0600

    Use macros for memory funtions

---
+++ include/jonallocators/memory_functions.h
@@ -0,0 +1,7 @@
+#ifndef JONALLOCATORS_MEMORY_FUNCTIONS_H
+#define JONALLOCATORS_MEMORY_FUNCTIONS_H
+
+#define jon_malloc malloc
+#define jon_free free
+
+#endif // JONALLOCATORS_MEMORY_FUNCTIONS_H
--- src/arena.c
+++ src/arena.c
@@ -1,10 +1,11 @@
 #include "jonallocators/arena.h"
+#include "jonallocators/memory_functions.h"
 #include <assert.h>
 #include <stdlib.h>
 
 jonArena jon_arena_create(size_t size) {
   jonArena result = {.data = NULL, .offset = NULL, .capacity = 0};
-  void *memory_block = malloc(size);
+  void *memory_block = jon_malloc(size);
   if (memory_block == NULL) {
     return result;
   }
@@ -17,7 +18,7 @@ jonArena jon_arena_create(size_t size) {
 
 void jon_arena_delete(jonArena *const arena) {
   assert(arena->capacity && arena->data);
-  free(arena->data);
+  jon_free(arena->data);
   arena->data = NULL;
   arena->offset = NULL;
   arena->capacity = 0;
